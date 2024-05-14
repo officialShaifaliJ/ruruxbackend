6f9cf375136fdd89324ef082b4404b4ff1803145
shaifali <shaifali02000@gmail.com>
2024-05-14 15:38:42 +0530

    latest commmit

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ app.post("/admin/login", async (req, res) => {
   const { username, password } = req.body;
   try {
     if (username === "admin" && password === "admin123") {
-      res.send({ message: "Admin login successfull" ,data:{username,password}});
+      res.send({
+        message: "Admin login successfull",
+        data: { username, password },
+      });
     } else {
       res.status(400).send({ message: "Invalid credentials" });
     }
@@ -23,7 +26,7 @@ app.post("/admin/login", async (req, res) => {
 app.post("/student/register", async (req, res) => {
   const student = new StudentModel({
     username: req.body.username,
-    enrollmentYear:req.body.enrollmentYear,
+    enrollmentYear: req.body.enrollmentYear,
     password: req.body.password,
   });
 
@@ -43,11 +46,15 @@ app.post("/student/register", async (req, res) => {
 
 app.post("/student/login", async (req, res) => {
   const { username, password } = req.body;
-  console.log("---------------------------------------->",username,password)
+  console.log("---------------------------------------->", username, password);
   try {
     const student = await StudentModel.findOne({ username, password });
-    res.json({ message: "Login successful", id: student._id, username ,student});
-    
+    res.json({
+      message: "Login successful",
+      id: student._id,
+      username,
+      student,
+    });
   } catch (err) {
     res.status(400).send({ message: err.message });
   }
@@ -62,64 +69,126 @@ app.get("/allstudents/list", async (req, res) => {
   }
 });
 
+app.get("/profile/:studentId", async (req, res) => {
+  const { studentId } = req.params;
+  try {
+    const student = await StudentModel.findOne(studentId);
+    res.status(200).send(student);
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
+});
+
 app.delete("/student/:studentId", async (req, res) => {
-  const {studentId} = req.params;
+  const { studentId } = req.params;
   try {
     const student = await StudentModel.findByIdAndDelete(studentId);
     if (!student) {
       return res.status(404).send({ message: "Student not found" });
     }
-    const student1= StudentModel.find()
+    const student1 = StudentModel.find();
     res.status(200).send({ message: "Student deleted successfully", student1 });
   } catch (err) {
     res.status(400).send({ message: err.message });
   }
 });
 
-app.post("/addstream",async(req,res)=>{
-  const {username,stream}=req.body;
-  try{
-const user = await StudentModel.findOne({username});
-if(user){
-  user.field.push(stream);
-  await user.save();
-  res.status(200).send(user)
-}else{
-  res.status(400).send({message:"user not found"})
-}
+app.post("/addstream", async (req, res) => {
+  const { username, stream } = req.body;
+  try {
+    const user = await StudentModel.findOne({ username });
+    if (user) {
+      user.field.push(stream);
+      await user.save();
+      res.status(200).send(user);
+    } else {
+      res.status(400).send({ message: "user not found" });
+    }
+  } catch (error) {
+    res.status(400).send({ message: error.message });
   }
-  catch(error){
-    res.status(400).send({message:error.message})
+});
+
+app.post("/addMarks", async (req, res) => {
+  const { username, subject, marksobtained, field } = req.body;
+  try {
+    const newMarksData = {
+      subject,
+      marksobtained,
+      field,
+    };
+    const user = await StudentModel.findOne({ username });
+
+    if (user) {
+      const existingMarkIndex = user.marks.findIndex(
+        (mark) => mark.subject === subject || mark.field === field
+      );
+      if (existingMarkIndex !== -1) {
+        user.marks[existingMarkIndex].marksobtained = marksobtained;
+      } else {
+        user.marks.push(newMarksData);
+      }
+
+      // user.marks.push(newMarksData);
+      if (!user.subject.includes(subject)) {
+        user.subject.push(subject);
+      }
+      if (!user.field.includes(field)) {
+        user.field.push(field);
+      }
+      await user.save();
+      res.status(200).send(user);
+    } else {
+      res.status(400).send({ message: "user not found" });
+    }
+  } catch (error) {
+    res.status(400).send({ message: error.message });
   }
-})
+});
 
-app.patch("/removestream",async(req,res)=>{
-  const {username,stream}=req.body;
-  try{
-const user = await StudentModel.findOne({username});
-if(user){
-  user.field.remove(stream);
-  await user.save();
-  res.status(200).send(user)
-}else{
-  res.status(400).send({message:"user not found"})
-}
+app.patch("/removeMarks", async (req, res) => {
+  const { username, subject, field } = req.body;
+  try {
+    const user = await StudentModel.findOne({ username });
+
+    if (user) {
+      const markIndexToRemove = user.marks.findIndex(
+        (mark) => mark.subject === subject
+      );
+
+      if (markIndexToRemove !== -1) {
+        user.marks.splice(markIndexToRemove, 1);
+        user.marks.remove(markIndexToRemove);
+        await user.save();
+        res.status(200).send(user);
+      } else {
+        res
+          .status(404)
+          .send({ message: "Marks for the specified subject not found" });
+      }
+    } else {
+      res.status(400).send({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(400).send({ message: error.message });
   }
-  catch(error){
-    res.status(400).send({message:error.message})
+});
+
+app.patch("/removestream", async (req, res) => {
+  const { username, stream } = req.body;
+  try {
+    const user = await StudentModel.findOne({ username });
+    if (user) {
+      user.field.remove(stream);
+      await user.save();
+      res.status(200).send(user);
+    } else {
+      res.status(400).send({ message: "user not found" });
+    }
+  } catch (error) {
+    res.status(400).send({ message: error.message });
   }
-})
-
-
-
-
-
-
-
-
-
-
-
+});
 
 
 
