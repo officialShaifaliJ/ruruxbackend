f5237e9db638994d9f6f0f89fa69e458e5f80794
shaifali <shaifali02000@gmail.com>
2024-05-15 09:40:05 +0530

    mongoose link changed

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-const connection = mongoose.connect('mongodb://127.0.0.1:27017/rurux-university-dashboard')
+// const connection = mongoose.connect('mongodb://127.0.0.1:27017/rurux-university-dashboard')
+const connection = mongoose.connect('mongodb+srv://shaifali02000:shaifali123@cluster0.otc3adu.mongodb.net/')
 
 
 module.exports = {connection};
